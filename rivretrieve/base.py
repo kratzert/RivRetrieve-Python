4bf099e7f8fc5482f041fd306f16806c6edc6f78
Frederik Kratzert <f.kratzert@gmail.com>
2025-10-14 08:07:39 +0000

    Reformat

--- rivretrieve/base.py
+++ rivretrieve/base.py
@@ -53,7 +53,7 @@ class RiverDataFetcher(abc.ABC):
         """
         # Default implementation returns an empty DataFrame.
         # Subclasses should override this method if metadata is available.
-        return pd.DataFrame().set_index('gauge_id')
+        return pd.DataFrame().set_index("gauge_id")
 
     @staticmethod
     @abc.abstractmethod
