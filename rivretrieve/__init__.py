bec52e39a1915486a389cd5e5bab8238c1957d9c
thiagovmdon <thiagovmdon@gmail.com>
2025-10-29 16:05:23 +0100

    Add Germany-Berlin fetcher updated with ruff formating (Fixes #62)

--- rivretrieve/__init__.py
+++ rivretrieve/__init__.py
@@ -7,6 +7,7 @@ from .canada import CanadaFetcher
 from .chile import ChileFetcher
 from .czech import CzechFetcher
 from .france import FranceFetcher
+from .germany_berlin import GermanyBerlinFetcher
 from .japan import JapanFetcher
 from .norway import NorwayFetcher
 from .poland import PolandFetcher
@@ -17,6 +18,5 @@ from .spain import SpainFetcher
 from .uk_ea import UKEAFetcher
 from .uk_nrfa import UKNRFAFetcher
 from .usa import USAFetcher
-from .germany_berlin import GermanyBerlinFetcher
 
 __version__ = "0.1.0"
